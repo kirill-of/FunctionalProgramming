14e02653aae15cd4cfcbedbea4da974a791ab998
koficerov <koficerov@neoflex.ru>
2019-03-04 15:21:01 +0400

    Added task 2.15

---
+++ src/main/java/pro/ofitserov/stepik/functionalprogramming/practicallessons/t15/Step1.java
@@ -0,0 +1,25 @@
+package pro.ofitserov.stepik.functionalprogramming.practicallessons.t15;
+
+public class Step1 {
+
+    /**2.15 Test your knowledge of stream operations
+     *Any stream operations can be intermediate or terminal. The first is always lazy and return a new stream, the second is not lazy and return a value or produce a side-effect.
+
+     Distribute stream operations on these groups.
+     Table Problem — Tick the right boxes
+     All is correct.
+     Operations:	        Intermediate	Terminal
+     anyMatch                                  X
+     distinctX                   X
+     skip                        X
+     count                                     X
+     average (LongStream)                      X
+     collect                                   X
+     map                        X
+     limit                      X
+     max                                       X
+     filter                     X
+     sorted                     X
+     reduce                                    X
+     */
+}
