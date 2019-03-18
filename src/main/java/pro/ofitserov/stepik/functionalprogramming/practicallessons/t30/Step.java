f5b21af6883d5bd0d29994aceccd74c0309a033b
koficerov <koficerov@neoflex.ru>
2019-03-18 19:10:15 +0400

    Added task 2.30

---
+++ src/main/java/pro/ofitserov/stepik/functionalprogramming/practicallessons/t30/Step.java
@@ -0,0 +1,15 @@
+package pro.ofitserov.stepik.functionalprogramming.practicallessons.t30;
+
+/**
+ * 2.30 Myths and facts about parallel streams
+ * Select all valid statements about parallel streams.
+ * <p>
+ * [-] If we call a method parallel() twice we get more threads for the stream.
+ * [-] Parallel streams are always faster then sequential streams.
+ * [+] Stateless operations are better for parallel processing than stateful operations.
+ * [+] Performance of parallel streams depends on size of data, number of availabe cores, source datastructure and other factors.
+ * [+] In some cases the result of your code execution can be different after replacement a stream with a parallel stream.
+ * [+] A stream is either sequential or parallel, no mixed mode.
+ */
+public class Step {
+}
