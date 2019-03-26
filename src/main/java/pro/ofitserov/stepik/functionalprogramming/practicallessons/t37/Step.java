be818ccf6040984ed92b89f2913ca5857b289122
koficerov <koficerov@neoflex.ru>
2019-03-26 13:07:42 +0400

    Added task 2.37

---
+++ src/main/java/pro/ofitserov/stepik/functionalprogramming/practicallessons/t37/Step.java
@@ -0,0 +1,39 @@
+package pro.ofitserov.stepik.functionalprogramming.practicallessons.t37;
+
+/**
+ * 2.37 What monads exist in Java 8?
+ * Select all monads existing in Java 8.
+ * <p>
+ * Multiple Choice Quiz — Select one or more options from the list
+ * Right.
+ * You just solved a difficult problem, congratulations! You can help others in the comments below.
+ * Maybe
+ * Optional
+ * Option
+ * Try
+ * IO
+ * Either
+ * CompletableFuture
+ * Promise
+ * Stream
+ * Transactional
+ * Monad
+ */
+public class Step {
+
+// Existing
+//    Optional
+//    CompletableFuture
+//    Stream
+
+// Does not exist
+//    Maybe
+//    Option
+//    Try
+//    IO
+//    Either
+//    Promise
+//    Transactional
+//    Monad
+
+}
